ad3ad3dbd54058147df216ca400735b871ba6501
jcavar01 <julien.cavaro@iprospect.com>
2018-10-14 10:16:56 +0200

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ConfigurableJoint))]
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    private float speed = 3f;
+
+    [SerializeField]
+    private float xLookSensitivity = 3f;
+
+    [SerializeField]
+    private float yLookSensitivity = 3f;
+
+    [SerializeField]
+    private float thrusterForce = 1000f;
+
+    [Header("Joint Options")]
+    [SerializeField]
+    private JointDriveMode jointMode = JointDriveMode.Position;
+    [SerializeField]
+    private float jointSpring = 20f;
+    [SerializeField]
+    private float jointMaxForce = 40f;
+
+
+    private PlayerMotor motor;
+    private ConfigurableJoint joint;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+        joint = GetComponent<ConfigurableJoint>();
+
+        SetJointSetting(jointSpring);
+    }
+
+    private void Update()
+    {
+        float _xMove = Input.GetAxisRaw("Horizontal");
+        float _zMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _xMove;
+        Vector3 _moveVertical = transform.forward * _zMove;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+
+        motor.Move(_velocity);
+
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3  _rotation = new Vector3(0, _yRot, 0) * xLookSensitivity;
+
+        motor.Rotate(_rotation);
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        float _cameraRotationX = _xRot * yLookSensitivity;
+
+        motor.RotateCamera(_cameraRotationX);
+
+        Vector3 _thrusterForce = Vector3.zero;
+
+        if(Input.GetButton("Jump"))
+        {
+            _thrusterForce = Vector3.up * thrusterForce;
+
+            SetJointSetting(0f);
+        }
+        else
+        {
+            SetJointSetting(jointSpring);
+        }
+        motor.ApplyThruster(_thrusterForce);
+    }
+
+    private void SetJointSetting(float jointSpring)
+    {
+        joint.yDrive = new JointDrive
+        {
+            mode = jointMode,
+            maximumForce = jointMaxForce,
+            positionSpring = jointSpring
+        };
+    }
+}
---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+    [SerializeField]
+    private Behaviour[] componentToDisable;
+
+    Camera sceneCamera;
+
+    private void Start()
+    {
+        if(!isLocalPlayer)
+        {
+            foreach( Behaviour component in componentToDisable)
+                component.enabled = false;
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+
+            if(sceneCamera != null)
+            {
+                sceneCamera.transform.gameObject.SetActive(false);
+            }
+           
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.transform.gameObject.SetActive(true);
+        }
+    }
+}

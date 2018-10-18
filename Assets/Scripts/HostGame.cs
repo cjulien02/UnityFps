5ceda5ed69eac47895558b31c3f77e003e7fdace
jcavar01 <julien.cavaro@iprospect.com>
2018-10-18 09:01:13 +0200

    Add NetworkManager to prefabs

---
+++ Assets/Scripts/HostGame.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour {
+    [SerializeField]
+    private uint roomSize = 6;
+
+    private string roomName = "Default";
+
+    private NetworkManager networkManager;
+
+    public void Start()
+    {
+        networkManager = NetworkManager.singleton;
+
+        if(networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+    }
+
+    public void SetRoomSize(uint size)
+    {
+        roomSize = size;
+    }
+
+    public void SetRoomName(string name)
+    {
+        roomName = name;
+    }
+
+    public void  CreateRoom()
+    {
+        if(roomName != "" && roomName != null)
+        {
+            Debug.Log("Création de la partie " + roomName + "- " + roomSize + " slots");
+
+            networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+        }
+    }
+}
